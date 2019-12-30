564636726a2c744659b22d5d293ffe599bafbd7f
Fabricio Alegria <fabr9193@gmail.com>
2019-12-30 15:13:41 +0100

    back end v1

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cheap- Flight API
+
+##Use 
+
+#### /search GET
+* fly_from: IATA code (PAR [= Paris], FR [= France]) <br/>
+* date_from: date (08/08/2020) <br/>
+* adults: int (1)<br/>
+* price_to: int (90)<br/>
+* nights_in_dst_from: (5)
+
+
--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -6,10 +6,12 @@ use App\Entity\Research;
 use App\Form\SearchType;
 use http\Message\Body;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 
 class SearchController extends AbstractController
@@ -20,6 +22,7 @@ class SearchController extends AbstractController
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function  index(Request $request) {
+        //TODO: afficher le formulaire (EDIT : Ce sera surement en Vue dans un autre repository)
 
         // creates a task object and initializes some data for this example
         $research = new Research();
@@ -48,18 +51,70 @@ class SearchController extends AbstractController
      * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
      */
     public function  search(Request $request) {
-
+        //FIXME : gestion des erreurs 422 : mauvais nom de départ (n'accepte que Code pays[FR,IT,US] ou code aeroport[CDG, ORY, IAD])
+        //ici je récupère les arguments que j'enverrai à l'API
+        /**
+         * Paramètres en dur
+         * [
+         * 'flight_type' => 'round',
+         * 'partner' => 'picky',
+         * 'ret_from_diff_airport' => 0,
+         * ]
+         * Dates :
+         * date_from = date_to pour l'aller
+         * nights_in_dst_from = nights_in_dst_to pour le retour
+         */
         $link = 'https://api.skypicker.com/flights';
         $client = HttpClient::create();
-        $query = $request->getQueryString();
-        $response = $client->request('GET', $link ."?" .$query);
-        $contentType = $response->getHeaders()['content-type'][0];
+        $query = $this->fillQuery($request);
         $res = new Response();
-        $test = new Body();
-        $test->append($response->getContent());
-        $res->setStatusCode($response->getStatusCode());
-        $res->setContent($response->getContent());
-
+        try {
+            $response = $client->request('GET', $link . "?" . $query);
+            $contentType = $response->getHeaders()['content-type'][0];
+            $parsedContent = $this->parseContent($response->getContent());
+            $res->headers->set('content-type', $contentType);
+            $res->setStatusCode($response->getStatusCode());
+            $res->setContent(json_encode($parsedContent));
+        }
+        catch(ClientException $e){
+            $res->headers->set('content-type', 'application/json');
+            $res->setStatusCode($e->getCode());
+            if ($e->getCode() == 422) {
+                $res->setContent(json_encode(['Error' => 'Your fly_from should be either IATA code or 2-letter country code']));
+            }
+            else {
+                $res->setContent(json_encode(['Error' =>$e->getMessage()]));
+            }
+        }
         return $res;
+    }
+
+    private function parseContent(string $rawContent) : array {
+        //TODO : retourne les éléments pertinents (Nb total de vols, vol affiches par prix croissant et n'afficher que les destinations et le lien de booking (externe))
+        //TODO : on a les codes des airlines il faudrait récupérer les noms entiers AF = airfrance, DY = Norwegian
+        $content = json_decode($rawContent);
+        $totalResults = count($content->data);
+        $mapped = array_map( function($element){
+            return [
+                'city_from' => $element->cityFrom,
+                'city_to' =>$element->cityTo,
+                'price' => $element->price,
+                'route' => $element->routes,
+                'details' => $element->deep_link,
+            ];
+        },$content->data);
+        return ['total_results' => $totalResults, 'data' => $mapped];
+    }
+
+    private function fillQuery(Request $request) : string
+    {
+        $request->query->add([
+            'flight_type' => 'round',
+            'partner' => 'picky',
+            'ret_from_diff_airport' => 0,
+            'date_to' => $request->query->get('date_from'),
+            'nights_in_dst_to' => $request->query->get('nights_in_dst_from')
+            ]);
+        return http_build_query($request->query->all());
     }
 }
